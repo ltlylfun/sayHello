d7f854a07de38129e8f9084c6ad6e7bf6005b059
Fang ZhiJie <funjee126@qq.com>
2025-01-16 17:09:19 +0800

    添加用户资料更新和认证检查功能，集成Cloudinary上传，增加路由保护中间件

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { email, fullName, password } = req.body;
@@ -73,6 +74,36 @@ export const logout = (req, res) => {
     res.status(200).json({ message: "Logged out" });
   } catch (error) {
     console.log("error in logout controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userid = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile picture is required" });
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userid,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("error in updateProfile controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("error in checkAuth controller: ", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
