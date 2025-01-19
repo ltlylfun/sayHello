851d0801a20c6cb34447ffc75e1690ec3455c0fb
Fang ZhiJie <funjee126@qq.com>
2025-01-19 20:57:11 +0800

    添加路由和认证检查，更新App组件以支持用户导航和加载状态

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,52 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStory";
+import { useEffect } from "react";
+
+import { Rotation } from "@icon-park/react";
+
 const App = () => {
+  const { authUser, checkAuth, isCheckAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log({ authUser });
+  if (isCheckAuth && !authUser) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Rotation className="animate-spin" size="36" />
+      </div>
+    );
+  }
   return (
-    <div>
-      <button className="btn">Button</button>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </>
   );
 };
 export default App;
